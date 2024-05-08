3ac4286241e71ad565edda72be0397fb88f7e2af
Maria <131583333+MaryGrig@users.noreply.github.com>
2024-05-08 21:45:53 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+// alert('dzer 18 tarin lracele?')
+// let userName = prompt("name")
+// let userSurname = prompt("surename")
+// let user = confirm('dzer 18 tarin lracele?')
+// console.log(user);
+// alert('hello '+ userName + ' ' + userSurname)
+// console.log('hello '+ userName + ' ' + userSurname)
+// let x = 10
+// let y = '20'
+
+// console.log(x===y);
+
+// let age = +prompt('age')
+// let num = 10
+// console.log(age + num);
+
+// let age = +prompt('age')
+// // if(age > 18){
+// //     console.log('yes');
+// // } else{
+// //     console.log('no')
+// // }
+
+// if(age<18){
+//     console.log('no');
+// }else if(age>=18 && age<=30){
+//     console.log('ok')
+// }else{ 
+//     console.log('yes')
+// }
+
+// let x = 16
+// let y = x % 2
+
+// console.log(y);
+
+// let x = +prompt("number")
+// if(x % 2 ==0 && num !=0){
+//     console.log('zuyge')
+// }else if(x == 0){
+//     console.log('voch k voch z')
+// }else{
+//     console.log('kent')
+
+// // }
+
+// let age = +prompt('age')
+//  age >= 18?console.log('yes'): console.log('no')
+
+// let y = 10
+// let x = age >= 18? y = y + age: y = y - age;
+
+// console.log(x);
+
+let age = +prompt('number')
+switch(age){
+    case 10:
+        console.log('10 e');
+        break;
+    case 20:
+        console.log(20 + 'e');
+        break;
+    case 30:
+        console.log(age);
+        break;
+    default:
+        console.log('chgitenq');
+}
+
